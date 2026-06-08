028426ff3bf14d2fd2e0e8a5cbfcf0878b7ccf91
kirstenmmm <kirstenmjeremicz10@gmail.com>
2026-06-08 14:20:53 -0400

    simon says

---
+++ AP_CSA_Endyear_project/Assets/Scripts/SimonSaysManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class SimonSaysManager : MonoBehaviour
+{
+    public enum SimonColor{Red,Blue,Green,Yellow};
+    
+    void Start(){
+    
+    }
+    void Update(){
+        
+    }
+}
