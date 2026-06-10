7b8a73002644dc549470f478107ec8754d72f0fc
kirstenmmm <kirstenmjeremicz10@gmail.com>
2026-06-09 20:59:52 -0400

    Simon says final + other updates

--- AP_CSA_Endyear_project/Assets/Scripts/KeyButton.cs
+++ AP_CSA_Endyear_project/Assets/Scripts/KeyButton.cs
@@ -1,12 +1,25 @@
+using System.Collections;
 using UnityEngine;
 
 public class KeypadButton : MonoBehaviour
 {
     public string symbol;
-    public KeypadManager manager;
+    public KeypadManager keypadManager;
 
-    private void OnMouseDown()
-    {
-        manager.PressButton(symbol);
+    private Vector3 originalPos;
+
+    void Start(){
+        originalPos = transform.localPosition;
+    }
+
+    private void OnMouseDown(){
+        StartCoroutine(PressAnimation());
+        keypadManager.PressButton(symbol);
+    }
+
+    IEnumerator PressAnimation(){
+        transform.localPosition = originalPos + Vector3.down * 0.1f;
+        yield return new WaitForSeconds(0.1f);
+        transform.localPosition = originalPos;
     }
 }
