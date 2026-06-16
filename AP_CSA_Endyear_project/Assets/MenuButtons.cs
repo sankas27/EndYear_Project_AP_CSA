d7382f60b4a3c32f8cabdcb45c8dcd4d2c9113a4
patmos27-dot <patmos27@bergen.org>
2026-06-16 09:42:02 -0400

    oiooiioi
    iioi

--- AP_CSA_Endyear_project/Assets/MenuButtons.cs
+++ AP_CSA_Endyear_project/Assets/MenuButtons.cs
@@ -3,8 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuButton : MonoBehaviour
 {
-    private void OnMouseDown()
+    public void LoadPuzzles()
     {
         SceneManager.LoadScene("Puzzles");
+    }
+
+    public void GoToStartScreen()
+    {
+        Debug.Log("Back to start clicked");
+        SceneManager.LoadScene("Start_Screen");
+    }
+
+    public void LoadWinScreen()
+    {
+        SceneManager.LoadScene("Win_Screen");
+    }
+
+    public void LoadLoseScreen()
+    {
+        SceneManager.LoadScene("Lose_Screen");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
